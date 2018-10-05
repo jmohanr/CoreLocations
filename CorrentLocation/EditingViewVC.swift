246cf1239e4d34a6a4cf01d9b83e8ccb747559e1
Jagan <jmohanr33@gmail.com>
2018-10-05 17:51:56 +0530

    Added Bottom View

--- CorrentLocation/EditingViewVC.swift
+++ CorrentLocation/EditingViewVC.swift
@@ -15,13 +15,16 @@ struct tableViewData {
 import UIKit
 import AVKit
 class EditingViewVC: UIViewController,AVPlayerViewControllerDelegate {
-var totalData = [tableViewData]()
+    @IBOutlet weak var deviceBtn: UIButton!
+    @IBOutlet weak var libraryBtn: UIButton!
+    var totalData = [tableViewData]()
 var count = Int()
     var isSelected = Bool()
     @IBOutlet weak var tableView: UITableView!
     var player = AVPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         totalData = [tableViewData(names: "House Entrence", Images: "music1", count: "5", temPerature: "35 C"),tableViewData(names: "Kitchen", Images: "music2", count: "9", temPerature: "30 C"),tableViewData(names: "Hall", Images: "music3", count: "2", temPerature: "25 C"),tableViewData(names: "Bed Room", Images: "music4", count: "0", temPerature: "28 C"),tableViewData(names: "Dining Hall", Images: "music5", count: "1", temPerature: "20 C"),tableViewData(names: "Singing Room", Images: "music6", count: "3", temPerature: "28 C"),tableViewData(names: "Balcony", Images: "music7", count: "4", temPerature: "32 C")]
     }
 
